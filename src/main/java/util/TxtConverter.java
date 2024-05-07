22bc90921fe75a32b62594ff23e69d2e4e323085
Helen <2041940772@qq.com>
2024-05-07 19:19:22 +0800

    新建项目

--- src/main/java/util/TxtConverter.java
+++ src/main/java/util/TxtConverter.java
@@ -1,18 +1,16 @@
 package util;
 
 import com.monitoring.index.annotation.ExportProperty;
-
+import com.monitoring.index.model.ReportMode;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 /**
  * @author Grace
@@ -21,9 +19,13 @@ import java.util.regex.Pattern;
 public class TxtConverter
 {
 	private static final String SOH_DELIMITER = "\u0001";
-	public static <T> List<T> parseTxtData(String filePath, Class<T> clazz)
+
+	//解析TXT数据文件
+	public static <T> ReportMode parseTxtData(String filePath, Class<T> clazz, Map<String,Integer> rowMap)
 	{
-		List<T> dataList = new ArrayList<>();
+		ReportMode reportMode=new ReportMode();
+		Map<String,String> map=new HashMap<>();
+		//将每一行有效数据读取出来
 		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
 			String line;
 			while ((line = br.readLine()) != null) {
@@ -36,7 +38,7 @@ public class TxtConverter
 					if (field.isAnnotationPresent(ExportProperty.class)) {
 						ExportProperty annotation = field.getAnnotation(ExportProperty.class);
 						Integer index = annotation.index();
-						if(index<=fieldArray.length){
+						if(index<fieldArray.length){
 							field.setAccessible(true);
 							field.set(t, fieldArray[index]);
 						}
@@ -44,7 +46,12 @@ public class TxtConverter
 					}
 
 				}
-				dataList.add(t);
+				Field field1 =t.getClass().getDeclaredField("metricsCode");
+				Field field2 =t.getClass().getDeclaredField("metricsValue");
+				String code= (String) field1.get(t);
+				String value= (String) field2.get(t);
+				map.put(code,value);
+
 
 			}
 		} catch (IOException e) {
@@ -58,12 +65,56 @@ public class TxtConverter
 		{
 			throw new RuntimeException(e);
 		}
-		return null;
-	}
+		catch (NoSuchFieldException e)
+		{
+			throw new RuntimeException(e);
+		}
 
+		Map<Integer,Map<String,String>> result=parseLineNumber(map,rowMap);
+		reportMode.setResultMap(result);
+		return reportMode;
+	}
+   //将TXT文件 行数据按SOH间隔符分割
 	public static String[] getArray(String line)
 	{
 		String[] result = line.split(SOH_DELIMITER);
+		return result;
+	}
+	//将txt文件里map解析
+	public static Map<Integer,Map<String,String>> parseLineNumber(Map<String,String> map,Map<String,Integer> rowMap)
+	{
+		Map<Integer,Map<String,String>> result = new TreeMap<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer k1, Integer k2) {
+				// 对于字符串key升序排序
+				return k1.compareTo(k2);
+				// 若需要降序排序，则可以改为
+				// return k2.compareTo(k1);
+			}
+		});
+
+		map.forEach((key,value)->{
+			Map<String,String> rowData=new HashMap<>();
+			String[] keys=key.split(".");
+
+			//列
+			String column=keys[keys.length-1];
+			String rowKey=keys[0]+"."+keys[1]+"."+keys[2];
+			//行
+			Integer row=rowMap.get(rowKey);
+			rowData.put(column,value);
+			if(result.get(row)==null)
+			{
+				result.put(row,rowData);
+			}else
+			{
+				result.get(row).putAll(rowData);
+			}
+
+
+
+		});
+
 		return result;
 	}
 }
