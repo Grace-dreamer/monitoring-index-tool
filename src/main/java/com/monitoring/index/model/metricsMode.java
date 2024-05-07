52b98f5598abd914e288a0767a90ac8e42208659
Helen <2041940772@qq.com>
2024-05-07 19:20:04 +0800

    新建项目

---
+++ src/main/java/com/monitoring/index/model/metricsMode.java
@@ -0,0 +1,24 @@
+package com.monitoring.index.model;
+
+import com.monitoring.index.annotation.ExportProperty;
+import lombok.Data;
+
+/**
+ * @author Grace
+ * @date 2024/4/28 - 11:36
+ */
+@Data
+public class metricsMode
+{
+    private String RepCode;
+    private String ReportDate;
+    private String VersionId;
+    private String Range;
+    private String Currency;
+    private String Unit;
+    @ExportProperty(index = 7)
+    private String metricsCode;
+    @ExportProperty(index = 11)
+    private String metricsValue;
+
+}
