afd5e95c77e46853d831a2743532a4cebb9e9ecd
Helen <2041940772@qq.com>
2024-05-09 09:40:21 +0800

    新建项目

--- src/main/java/com/monitoring/index/model/ExcelMode.java
+++ src/main/java/com/monitoring/index/model/ExcelMode.java
@@ -1,16 +1,20 @@
 package com.monitoring.index.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import com.alibaba.excel.annotation.ExcelProperty;
 
 /**
  * @author Grace
  * @date 2024/5/9 - 9:26
  */
+@EqualsAndHashCode
 @Data
 public class ExcelMode
 {
+    @ExcelProperty(index = 0)
     private String metricsCode;
-
+    @ExcelProperty(index = 1)
     private String rowNumber;
 
 }
